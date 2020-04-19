07862662ebc80dbfb0fee846e298e6bd5218492e
Nozfearatoo <vs.vasilevskiy@gmail.com>
2020-04-19 15:57:17 +0300

    6Averg added testlogic added

---
+++ src/SixOperations.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SixOperations {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int num1 = in.nextInt();
+        System.out.print("Введите второе число: ");
+        int num2 = in.nextInt();
+        System.out.print("Введите третье число: ");
+        int num3 = in.nextInt();
+        int avrg = (num1+num2+num3)/3;
+        int avrgdiv = avrg/2;
+
+        System.out.println("Среднее арифметическое: " + avrg); //System.out.println(avrgdiv);
+        if (avrgdiv > 3) System.out.println("Программа выполнена корректно");
+    }
+}
---
+++ src/TestLogic.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TestLogic {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число 1: ");
+        int num = in.nextInt();
+        if (num==1) {System.out.print("Вы ввели число 1");}
+    }
+}
