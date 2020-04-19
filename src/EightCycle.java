49d293214f151cfbf7e0ef26d36d281c04e37496
Nozfearatoo <vs.vasilevskiy@gmail.com>
2020-04-19 17:08:12 +0300

    EightCycle added

---
+++ src/EightCycle.java
@@ -0,0 +1,8 @@
+public class EightCycle {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 50; x++)
+        {
+            System.out.print(x + " ");
+        }
+    }
+}
